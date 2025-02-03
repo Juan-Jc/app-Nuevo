650bbad9215b1f0f6d33488ae1be75831550f22f
JuanP <juanp@yahoo.com>
2025-02-03 20:34:18 +0100

    creamos los getters y setters del usuario

--- src/Model/Usuario.php
+++ src/Model/Usuario.php
@@ -2,7 +2,8 @@
 
 namespace App\Model;
 
-class Usuario {
+class Usuario
+{
     private string $nombre;
     private string $apellido1;
     private string $apellido2;
@@ -12,31 +13,75 @@ class Usuario {
 
     public function __construct(string $name, string $ape1, string $ape2, string $email, int $tlf, string $pwd)
     {
-       $this->nombre = $name;
-       $this->apellido1= $ape1;
-       $this->apellido2= $ape2;
-       $this->email= $email;
-       $this->telefono= $tlf;
-       $this->pwd = $pwd; 
+        $this->nombre = $name;
+        $this->apellido1 = $ape1;
+        $this->apellido2 = $ape2;
+        $this->email = $email;
+        $this->telefono = $tlf;
+        $this->pwd = $pwd;
     }
 
-    public function getNombre():string{
+    public function getNombre(): string
+    {
         return $this->nombre;
     }
-    public function setNombre($nombre){
+    public function setNombre($nombre)
+    {
         $this->nombre = $nombre;
     }
-    public function getApellido1():string{
+    public function getApellido1(): string
+    {
         return $this->apellido1;
     }
-    public function setApellido1($apellido1){
+    public function setApellido1($apellido1)
+    {
         $this->apellido1 = $apellido1;
     }
-    public function getApellido2():string{
+    public function getApellido2(): string
+    {
         return $this->apellido2;
     }
-    public function setApellido2($apellido2){
+    public function setApellido2($apellido2)
+    {
         $this->apellido2 = $apellido2;
     }
 
+
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getTelefono(): int
+    {
+        return $this->telefono;
+    }
+
+
+    public function setTelefono(int $telefono): self
+    {
+        $this->telefono = $telefono;
+        return $this;
+    }
+
+
+    public function getPwd(): string
+    {
+        return $this->pwd;
+    }
+
+
+    public function setPwd(string $pwd): self
+    {
+        $this->pwd = $pwd;
+        return $this;
+    }
 }
